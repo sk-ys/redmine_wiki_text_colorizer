1c1d93f94d155344c2245d7a7c89e5e4ba130282
sk-ys <yskrmt@gmail.com>
2023-07-18 20:28:07 +0900

    Add empty button

--- assets/javascripts/wiki_text_colorizer.js
+++ assets/javascripts/wiki_text_colorizer.js
@@ -1,3 +1,24 @@
-$(function () {
-  console.log('_base.html.erb loaded');
-});
+(() => {
+  if (typeof jsToolBar === "undefined") return;
+
+  const textColorButton = {
+    type: "button",
+    title: "Text Color",
+    fn: {
+      wiki: function () {
+        console.log("action");
+      },
+    },
+  };
+
+  // Recreate toolbar
+  const elements = {};
+  for (const e in jsToolBar.prototype.elements) {
+    elements[e] = jsToolBar.prototype.elements[e];
+    if (e === "em") {
+      // Insert button to apply color to text after em
+      elements["text_color"] = textColorButton;
+    }
+  }
+  jsToolBar.prototype.elements = elements;
+})();
