sk-ys/redmine_issue_note_list

rails generate redmine_plugin

init.rb
@@ -1,0 +1,8 @@
+Redmine::Plugin.register :redmine_issue_note_list do
+  name 'Redmine Issue Note List plugin'
+  author 'sk-ys'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'https://github.com/sk-ys/redmine_issue_note_list'
+  author_url 'https://github.com/sk-ys'
+end

sk-ys/redmine_wiki_text_colorizer

init.rb
@@ -1,0 +1,8 @@
+Redmine::Plugin.register :redmine_wiki_text_colorizer do
+  name 'Redmine Wiki Text Colorier plugin'
+  author 'sk-ys'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'https://github.com/sk-ys/redmine_wiki_text_colorizer'
+  author_url 'https://github.com/sk-ys'
+end
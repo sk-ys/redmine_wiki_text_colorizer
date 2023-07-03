a58029e26904d6d8d95141c5911d95f09152f9a7
sk-ys <yskrmt@gmail.com>
2023-07-03 23:16:51 +0900

    rails generate redmine_plugin

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_wiki_text_colorizer do
+  name 'Redmine Wiki Text Colorier plugin'
+  author 'sk-ys'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'https://github.com/sk-ys/redmine_wiki_text_colorizer'
+  author_url 'https://github.com/sk-ys'
+end
