29a36acff8128fb9bd729befd87fb7f4d24ab273
sk-ys <yskrmt@gmail.com>
2023-07-31 08:02:02 +0900

    Update init.rb and readme description.

--- init.rb
+++ init.rb
@@ -3,7 +3,7 @@ require_dependency File.expand_path('../lib/wiki_text_colorizer/hooks.rb', __FIL
 Redmine::Plugin.register :redmine_wiki_text_colorizer do
   name 'Redmine Wiki Text Colorier plugin'
   author 'sk-ys'
-  description 'This is a plugin for Redmine'
+  description 'Add buttons to jsToolBar for changing text and background colors.'
   version '0.1.0'
   url 'https://github.com/sk-ys/redmine_wiki_text_colorizer'
   author_url 'https://github.com/sk-ys'
